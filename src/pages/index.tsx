8b64c0e501d5f08f7d95502850085bdfe6210c68
weilemann <yuri.weilemann@impar.com.br>
2021-09-01 16:46:43 -0300

    Created global style

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/Head';
+import styles from '../styles/home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>
+        Hello World
+      </h1>
+    </>
+  )
+}
