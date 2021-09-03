1537f9e1dd0a73795d57938c5a0fbacd369f62c0
weilemann <yuri_weilemann@hotmail.com>
2021-09-03 15:21:09 -0300

    Added more events to be listened by webhooks

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { session } from 'next-auth/client';
 import { Readable } from 'stream';
 import Stripe from 'stripe';
 import { stripe } from '../../services/stripe'
@@ -24,7 +23,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',
+    'customer.subscription.updated',
+    'customer.subscription.deleted',
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -45,21 +46,33 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         if(relevantEvents.has(type)) {
             try {
                 switch(type) {
+                    case 'customer.subscription.updated':
+                    case 'customer.subscription.deleted':
+                        const subscription = event.data.object as Stripe.Subscription;
+
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            false,
+                        );
+
+                        break;
                     case 'checkout.session.completed':
                         const checkoutSession = event.data.object as Stripe.Checkout.Session;
 
                         await saveSubscription(
                             checkoutSession.subscription.toString(),
                             checkoutSession.customer.toString(),
+                            true,
                         )
 
                         break;
                     default:
-                        throw new Error('Unhandled event.')
+                        throw new Error('Unhandled event.');
                 }
             }
             catch(err) {
-                return res.json({ error: 'Webhook handler failed.'})
+                return res.json({ error: 'Webhook handler failed.'});
             }
         }
 
